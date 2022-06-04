d5ef057a8add2ef8c362b4553f6d9da523fe0864
Prashant Pandey <103842494+prashant-i3@users.noreply.github.com>
2022-06-04 21:34:40 +0530

    Add files via upload

---
+++ MERN/05_JS Concepts/async.md
@@ -0,0 +1,7 @@
+Event Driven Architecture   OR Non-Blocking architecture in node js
+
+
+//Giving random output.........WHY???
+
+ANSWER:-
+
---
+++ MERN/05_JS Concepts/asyncParallel.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+
+// const { findSeries } = require("async")
+
+// console.log('Before')
+
+// let data1 = fs.readFileSync('f1.txt')
+// let data2 = fs.readFileSync('f2.txt')
+// let data3 = fs.readFileSync('f3.txt')
+
+// console.log('This is file Data of file 1' + data1)
+// console.log('This is file Data of file 2' + data2)
+// console.log('This is file Data of file 3' + data3)
+
+console.log('Before')
+
+fs.readFile('f1.txt', cb)
+
+function cb(err, data){
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log('This is file data->  ' +data)
+    }
+}
+
+fs.readFile('f2.txt', cb)
+
+function cb(err, data){
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log('This is file data->  ' +data)
+    }
+}
+
+fs.readFile('f3.txt', cb)
+
+function cb(err, data){
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log('This is file data->  ' +data)
+    }
+}
+
+
+
+console.log('After')
