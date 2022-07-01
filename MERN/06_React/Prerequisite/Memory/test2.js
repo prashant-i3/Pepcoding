36707d4b13250e4fe9632b89176eb9f0ff722ce3
Prashant Pandey <103842494+prashant-i3@users.noreply.github.com>
2022-07-01 21:20:22 +0530

    Add files via upload

---
+++ MERN/06_React/Prerequisite/Memory/test2.js
@@ -0,0 +1,29 @@
+let firstPerson = {
+    name: "Adam",
+    age: 23,
+    address: {
+      country: "India",
+      city: {
+        cityName: "Delhi",
+        pincode: 123456,
+      },
+    },
+  
+    test: {
+      value1: 2,
+    },
+  };
+  
+  let secondPerson = { ...firstPerson }; // shallow copy
+  
+  // firstPerson.address.country = "USA";
+  
+  // firstPerson.address.city.cityName='Mumbai'
+  
+  let thirdPerson = JSON.parse(JSON.stringify(firstPerson)); // deep copy
+  
+  firstPerson.address.city.pincode = "56789";
+  
+  console.log(firstPerson);
+  console.log(secondPerson);
+  console.log(thirdPerson);
